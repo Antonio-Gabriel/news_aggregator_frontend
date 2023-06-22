5254b887120f71c994b8dc7989bc9a4abf604f01
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-22 10:10:56 +0100

    feat: configure custom filters for user settings

--- src/components/common/custom-filters-modal.tsx
+++ src/components/common/custom-filters-modal.tsx
@@ -1,9 +1,19 @@
-import { FormEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+
 import { Modal } from 'reactstrap'
 import { Button } from './button'
 
-import { RootState } from '../../app/store'
+import { Article } from '../../core/domain/entities/article'
+import { useCustomMapper } from '../../hooks/use-custom-mapper'
+import { useArticlesExtractor } from '../../hooks/articles/useArticlesExtractor'
+
+import { hasSubsetArray } from '../../utils/has-subset-array'
+import { useMapCustomDefinitions } from '../../hooks/use-map-custom-definitions'
+
 import { useSelector } from 'react-redux'
+import { RootState } from '../../app/store'
+
+import { getClientUserCookie } from '../../utils/cookies'
 
 import './styles/modal.scss'
 
@@ -12,16 +22,152 @@ type CustomFiltersModalProps = {
   toggleCustomFiltersModal: () => void
 }
 
-export function CustomFiltersModal(props: CustomFiltersModalProps) {
-  const [focusAfterClose, setFocusAfterClose] = useState(true)
+type AuthenticationProps = Authentication.Module.AuthenticationProps
 
-  const { categories, sources, authors } = useSelector(
-    (state: RootState) => state.dataExtracteds,
-  )
+type FilterSettings = {
+  authors: string[]
+  categories: string[]
+  sources: string[]
+}
+
+export function CustomFiltersModal(props: CustomFiltersModalProps) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [focusAfterClose, setFocusAfterClose] = useState(true)
+  const [hasSettingChange, setHasSettingChange] = useState(false)
+  const [filterSettings, setFilterSettings] = useState<FilterSettings>({
+    authors: [],
+    categories: [],
+    sources: [],
+  })
+
+  const { data } = useArticlesExtractor()
+  const { isAuth } = useSelector((state: RootState) => state.auth)
+  const { categories, authors, sources } = useCustomMapper(data as Article[])
+
+  const { savedSettings } = useMapCustomDefinitions()
+
+  useEffect(() => {
+    if (savedSettings != null) {
+      const setting = savedSettings
+      setFilterSettings({
+        authors: [...setting.authors],
+        categories: [...setting.categories],
+        sources: [...setting.sources],
+      })
+    }
+  }, [savedSettings])
+
+  useEffect(() => {
+    if (!isAuth) {
+      resetComponentStates()
+    }
+  }, [isAuth])
+
+  function removeSetting(index: number, key: string) {
+    setHasSettingChange(true)
+
+    setFilterSettings((prevSettings) => {
+      // @ts-ignore
+      const newList = [...prevSettings[key]]
+      newList.splice(index, 1)
+
+      return {
+        ...prevSettings,
+        [key]: newList,
+      }
+    })
+  }
+
+  function joinElements(element: any[]) {
+    return element.join(',')
+  }
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault()
+
+    setIsLoading(true)
     setFocusAfterClose(false)
+
+    const authorFilters = joinElements(filterSettings.authors)
+    const sourceFilters = joinElements(filterSettings.sources)
+    const categoriesFilters = joinElements(
+      filterSettings.categories.map((arr) => arr[0]),
+    )
+
+    let userClientData = getClientUserCookie<AuthenticationProps>(
+      '@news-aggregator',
+    )
+
+    const metadata = {
+      authors: authorFilters,
+      categories: categoriesFilters,
+      sources: sourceFilters,
+    }
+
+    const refreshUserClientCookie = {
+      ...userClientData,
+      settings: {
+        id: userClientData?.settings?.id,
+        metadata: JSON.stringify(metadata),
+      },
+    }
+
+    console.log(refreshUserClientCookie)
+  }
+
+  function handleSelectChange(e: ChangeEvent<HTMLSelectElement>) {
+    e.preventDefault()
+    const selectedValues = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value,
+    )
+
+    if (
+      // @ts-ignore
+      !filterSettings[e.target.name].includes(e.target.value) &&
+      e.target.value !== '0'
+    ) {
+      setFilterSettings((prevFilterSettings) => ({
+        ...prevFilterSettings,
+        [e.target.name]: [
+          // @ts-ignore
+          ...prevFilterSettings[e.target.name],
+          ...selectedValues,
+        ],
+      }))
+    }
+
+    setHasSettingChange(true)
+  }
+
+  function handleSelectMultipleChange(e: ChangeEvent<HTMLSelectElement>) {
+    const selectedValue = e.target.value
+    const selectedArray = selectedValue.split(',')
+
+    if (
+      // @ts-ignore
+      !hasSubsetArray(filterSettings[e.target.name], selectedArray) &&
+      e.target.value !== '0'
+    ) {
+      setFilterSettings((prevFilterSettings) => ({
+        ...prevFilterSettings,
+        [e.target.name]: [
+          // @ts-ignore
+          ...prevFilterSettings[e.target.name],
+          selectedArray,
+        ],
+      }))
+    }
+
+    setHasSettingChange(true)
+  }
+
+  function resetComponentStates() {
+    setFilterSettings({
+      authors: [],
+      categories: [],
+      sources: [],
+    })
   }
 
   return (
@@ -41,66 +187,66 @@ export function CustomFiltersModal(props: CustomFiltersModalProps) {
             className="btn-close"
             data-bs-dismiss="modal"
             aria-label="Close"
-            onClick={props.toggleCustomFiltersModal}
+            onClick={() => {
+              setHasSettingChange(false)
+              props.toggleCustomFiltersModal()
+            }}
           ></button>
         </div>
 
         <form onSubmit={handleSubmit}>
           <div className="modal-body">
             <div className="form-group">
-              <select className="form-select form-select-custom">
-                <option value="1">Choose your prefered categories</option>
+              <select
+                name="categories"
+                onChange={handleSelectMultipleChange}
+                className="form-select form-select-custom"
+              >
+                <option value="0">Choose your prefered categories</option>
                 {categories.map((category) => (
-                  <option value={category.id} key={category.id}>
+                  <option
+                    value={`${category.id},${category.name}`}
+                    key={category.id}
+                  >
                     {category.name}
                   </option>
                 ))}
               </select>
 
               <ul className="nav-list">
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Lifehacker.com
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Engadget
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
+                {filterSettings.categories.map((item, index) => (
+                  <li className="nav-item" key={index}>
+                    <a href="#" className="nav-link">
+                      {item[1]}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        onClick={() => {
+                          removeSetting(index, 'categories')
+                        }}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="form-group">
-              <select className="form-select form-select-custom">
-                <option value="1">Choose your prefered authors</option>
+              <select
+                name="authors"
+                onChange={handleSelectChange}
+                className="form-select form-select-custom"
+              >
+                <option value="0">Choose your prefered authors</option>
                 {authors.map((author, index) => (
                   <option value={author} key={index}>
                     {author}
@@ -109,68 +255,39 @@ export function CustomFiltersModal(props: CustomFiltersModalProps) {
               </select>
 
               <ul className="nav-list">
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Steve Dants
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Clerlynn Low{' '}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Andrew Tarantola
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
+                {filterSettings.authors.map((author, index) => (
+                  <li className="nav-item" key={index}>
+                    <a href="#" className="nav-link">
+                      {author}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        onClick={() => {
+                          removeSetting(index, 'authors')
+                        }}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="form-group">
-              <select className="form-select form-select-custom">
-                <option>Choose your prefered sources</option>
+              <select
+                name="sources"
+                onChange={handleSelectChange}
+                className="form-select form-select-custom"
+              >
+                <option value="0">Choose your prefered sources</option>
                 {sources.map((source, index) => (
                   <option value={source} key={index}>
                     {source}
@@ -179,52 +296,48 @@ export function CustomFiltersModal(props: CustomFiltersModalProps) {
               </select>
 
               <ul className="nav-list">
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    Innocation & Development
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="" className="nav-link">
-                    ABC News
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </a>
-                </li>
+                {filterSettings.sources.map((source, index) => (
+                  <li className="nav-item" key={index}>
+                    <a href="#" className="nav-link">
+                      {source}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        onClick={() => {
+                          removeSetting(index, 'sources')
+                        }}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
 
           <div className="model-footer">
-            <Button variant="primary" isAction={false}>
-              Save Filters
-            </Button>
+            {isLoading ? (
+              <Button variant="primary" isAction={false} disabled={true}>
+                Loading...
+              </Button>
+            ) : (
+              <Button
+                variant="primary"
+                isAction={false}
+                disabled={hasSettingChange ? false : true}
+              >
+                {hasSettingChange ? 'Save Filters' : 'No Changes identified'}
+              </Button>
+            )}
           </div>
         </form>
       </div>
