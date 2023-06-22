54e2ef43bca7a4444e7619c5b09a48133edb1794
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-06-22 10:11:32 +0100

    chore: remove unused variables

--- src/app/feactures/data-extractor-slice.ts
+++ src/app/feactures/data-extractor-slice.ts
@@ -3,19 +3,16 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 type StateProps = {
   categories: App.Module.CategoryProps[]
   sources: string[]
-  authors: string[]
 }
 
 type ActionPayload = {
   categories: App.Module.CategoryProps[]
   sources: string[]
-  authors: string[]
 }
 
 const initialState: StateProps = {
   categories: [],
   sources: [],
-  authors: [],
 }
 
 const dataExtractorSlice = createSlice({
@@ -24,7 +21,6 @@ const dataExtractorSlice = createSlice({
   reducers: {
     extractDataFromArticles(state, payload: PayloadAction<ActionPayload>) {
       state.categories = payload.payload.categories
-      state.authors = payload.payload.authors
       state.sources = payload.payload.sources
     },
   },
